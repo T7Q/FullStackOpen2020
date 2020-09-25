f01e5405a306e8e4b04212b1d3057f2209229747
T7Q <tkuumola@gmail.com>
2020-09-25 11:00:33 +0300

    feat: :zap: 1.11*

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,75 +1,56 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const Header = ({text}) => {
-  return (
-      <h1>
-        {text}
-      </h1>
-  )
-}
+const Header = ({ text }) => <h1>{text}</h1>;
 
-const Button = ({ onClick, text}) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const Stat = ({text, value }) => {
-  if (text === 'positive') {
+const Statistic = ({ text, value }) => {
     return (
-      <p>
-        {text} {value}%
-      </p>
-    )
-  }
-  return (
-    <p>
-      {text} {value}
-    </p>
-  )
-}
+        <tr>
+            <td>{text} </td>
+            <td> {value} {text === "positive" ? "%" : ""} </td>
+        </tr>
+    );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+    const all = good + neutral + bad;
+    const average = (good - bad) / all;
+    const positive = (good * 100) / all;
+    if (all < 1) {
+        return <p>No feedback given</p>;
+    }
+    return (
+        <table>
+            <tbody>
+                <Statistic text="good" value={good} />
+                <Statistic text="neutral" value={neutral} />
+                <Statistic text="bad" value={bad} />
+                <Statistic text="all" value={all} />
+                <Statistic text="average" value={average} />
+                <Statistic text="positive" value={positive} />
+            </tbody>
+        </table>
+    );
+};
 
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-  const haddleGoodClick = () => {
-    setAll(all + 1);
-    setGood(good + 1);
-  }
-  const haddleBadClick = () => {
-    setAll(all + 1);
-    setBad(bad + 1);
-  }
-  const haddleNeutralClick = () => {
-    setAll(all + 1);
-    setNeutral(neutral + 1);
-  }
+    return (
+        <div>
+            <Header text="give feedback" />
+            <Button onClick={() => {setGood(good + 1)}} text="good" />
+            <Button onClick={() => {setNeutral(neutral + 1)}} text="neutral" />
+            <Button onClick={() => {setBad(bad + 1)}} text="bad" />
+            <Header text="statistics" />
+            <Statistics good={good} neutral={neutral} bad={bad} />
+        </div>
+    );
+};
 
-  return (
-    <div>
-      <Header text='give feedback'/>
-      <Button onClick={haddleGoodClick} text='good'/>
-      <Button onClick={haddleNeutralClick} text='neutral'/>
-      <Button onClick={haddleBadClick} text='bad'/>
-      <Header text='statistics'/>
-      <Stat text='good' value={good}/>
-      <Stat text='neutral' value={neutral}/>
-      <Stat text='bad' value={bad}/>
-      <Stat text='all' value={all}/>
-      <Stat text='average' value={(good * 1 + bad * (-1)) / all}/>
-      <Stat text='positive' value={(good * 100) / all }/>
-
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
