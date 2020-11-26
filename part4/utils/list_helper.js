194ccaebf113e06e703b9fc20f79582ffa765cfe
T7Q <tkuumola@gmail.com>
2020-11-26 21:37:46 +0200

    feat: :zap: part4 ex. 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,18 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
 	return 1;
 };
 
+const totalLikes = blogs => {
+	return blogs.reduce((sum, value) => sum + value.likes, 0) || 0
+}
+
+const favoriteBlog = blogs => {
+}
+
 module.exports = {
 	dummy,
+	totalLikes,
+	favoriteBlog
 };
