07a7357093f478cb256a557a769e2ebb50575c0c
T7Q <tkuumola@gmail.com>
2020-11-27 14:34:37 +0200

    feat: :zap: part4 ex. 4.11*

--- part4/tests/blog.test.js
+++ part4/tests/blog.test.js
@@ -53,15 +53,23 @@ test('request creates a new blog post', async () => {
     likes: 10,
   }
 
-  await api
-    .post('/api/blogs')
-    .send(newPost)
-    .expect(201)
+  await api.post('/api/blogs').send(newPost).expect(201)
 
   const response = await api.get('/api/blogs')
 
   expect(response.body.length).toBe(testBlogs.length + 1)
   expect(response.body).toContainEqual(expect.objectContaining(newPost))
+})
+test('likes property set to 0 if missing', async () => {
+  const newPost = {
+    title: 'Hello world',
+    author: 'Joe Doe',
+    url: 'https://newpost.com/',
+  }
+
+  const savedPost = await api.post('/api/blogs').send(newPost)
+
+  expect(savedPost.body).toHaveProperty('likes', 0)
 })
 
 afterAll(() => {
