ad5a64d2b7f4aa227f363267fd16dfaffd100975
T7Q <tkuumola@gmail.com>
2020-11-27 14:23:55 +0200

    feat: :zap: part4 ex. 4.10

--- part4/tests/blog.test.js
+++ part4/tests/blog.test.js
@@ -9,25 +9,16 @@ const api = supertest(app)
 
 const testBlogs = [
   {
-    _id: '5a422a851b54a676234d17f7',
     title: 'React patterns',
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
     likes: 7,
   },
   {
-    _id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 3,
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 10,
   },
 ]
 
@@ -52,6 +43,25 @@ test('correct number of blogs are returned', async () => {
 test('unique identifier property of the blog posts', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body[0].id).toBeDefined()
+})
+
+test('request creates a new blog post', async () => {
+  const newPost = {
+    title: 'New post',
+    author: 'Jane Doe',
+    url: 'https://newpost.com/',
+    likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newPost)
+    .expect(201)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body.length).toBe(testBlogs.length + 1)
+  expect(response.body).toContainEqual(expect.objectContaining(newPost))
 })
 
 afterAll(() => {
