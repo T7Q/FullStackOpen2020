454980f3e6e9abdad79819d8866eb654ccd901a0
T7Q <tkuumola@gmail.com>
2020-11-27 13:48:31 +0200

    feat: :zap: part4 ex. 4.8

---
+++ part4/tests/blog.test.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const testBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 3,
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 10,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await new Blog(testBlogs[0]).save()
+  await new Blog(testBlogs[1]).save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct number of blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body.length).toBe(2)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
