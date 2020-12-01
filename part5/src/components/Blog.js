8882b37708ea61a62733a994e0c8fe80b43d3af4
T7Q <tkuumola@gmail.com>
2020-12-01 18:02:26 +0200

    feat: :zap: part5 ex. 5.7*

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 5,
+    border: '1px solid blue',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}{' '}
+        <button onClick={() => setVisible(!visible)}>{visible ? 'hide' : 'view'}</button>
+      </div>
+      {visible && (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
