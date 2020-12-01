1cd62f8b3558bc80464ee6b62bfcca04d8589be2
T7Q <tkuumola@gmail.com>
2020-12-01 21:22:18 +0200

    feat: :zap: part6 ex. 6.12

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,15 +4,20 @@ import { addVote } from '../reducers/anecdoteReducer'
 import { setNotification, clearNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state.anecdotes.sort((a, b) => b.votes - a.votes))
+  // const anecdotes = useSelector((state) => state.anecdotes.sort((a, b) => b.votes - a.votes))
+  const anecdotes = useSelector(({ filter, anecdotes }) =>
+    filter === ''
+      ? anecdotes
+      : anecdotes.filter((one) => one.content.toLowerCase().includes(filter.toLowerCase()))
+  )
   const dispatch = useDispatch()
 
   const vote = (anecdote) => {
     dispatch(addVote(anecdote.id))
-    dispatch(setNotification(`you voted '${anecdote.content}'`));
+    dispatch(setNotification(`you voted '${anecdote.content}'`))
     setTimeout(() => {
-      dispatch(clearNotification());
-    }, 5000);
+      dispatch(clearNotification())
+    }, 5000)
   }
 
   return (
