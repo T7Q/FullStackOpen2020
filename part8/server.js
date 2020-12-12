1e3a67b2b4f61d07e325360f8dee9e6403677587
T7Q <tkuumola@gmail.com>
2020-12-12 19:34:31 +0200

    feat: :zap: part8 ex. 8.5 updated

--- part8/server.js
+++ part8/server.js
@@ -112,15 +112,17 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-
-      if (args.author) {
-        return books.filter((book) => book.author === args.author)
+      if(args.author && args.genre) {
+        return books.filter(book => book.author === args.author && book.genres.includes(args.genre))
+      } else {
+        if (args.author) {
+          return books.filter((book) => book.author === args.author)
+        }
+        if (args.genre) {
+          return books.filter((book) => book.genres.includes(args.genre));
+        }
       }
-      if (args.genre) {
-        return books.filter((book) => book.genres.includes(args.genre));
-      }
-      return books
-      
+      return books      
     },
     allAuthors: () => authors,
   },
