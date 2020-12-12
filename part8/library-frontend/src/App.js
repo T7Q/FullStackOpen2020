c40a13d00cecc1e66fc8b92b0d0636e8960bbe65
T7Q <tkuumola@gmail.com>
2020-12-12 23:08:15 +0200

    feat: :zap: part8 ex. 8.9

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,14 +1,14 @@
-
 import React, { useState } from 'react'
-import { useQuery } from '@apollo/client'
+import { useQuery, useLazyQuery } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
-import { ALL_AUTHORS } from './queries';
+import { ALL_AUTHORS, ALL_BOOKS } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
-  const authors = useQuery(ALL_AUTHORS);
+  const authors = useQuery(ALL_AUTHORS)
+  const books = useQuery(ALL_BOOKS)
 
   return (
     <div>
@@ -18,19 +18,11 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors
-        show={page === 'authors'}
-        authors={authors}
-      />
+      <Authors show={page === 'authors'} authors={authors} />
 
-      <Books
-        show={page === 'books'}
-      />
+      <Books show={page === 'books'} books={books}/>
 
-      <NewBook
-        show={page === 'add'}
-      />
-
+      <NewBook show={page === 'add'} />
     </div>
   )
 }
