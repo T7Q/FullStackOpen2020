f49dcef810ff9f86dc43efbacbc5f8fd16fb77c1
T7Q <tkuumola@gmail.com>
2020-12-12 19:31:08 +0200

    feat: :zap: part8 ex. 8.5

--- part8/server.js
+++ part8/server.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -112,11 +112,15 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
+
       if (args.author) {
         return books.filter((book) => book.author === args.author)
-      } else {
-        return books
       }
+      if (args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre));
+      }
+      return books
+      
     },
     allAuthors: () => authors,
   },
