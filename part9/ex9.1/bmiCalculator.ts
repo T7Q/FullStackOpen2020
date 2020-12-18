5d2409f6017851205457db2291ebaee70e585518
T7Q <tkuumola@gmail.com>
2020-12-18 20:40:57 +0200

    feat: :zap: part9 ex. 9.5

--- part9/ex9.1/bmiCalculator.ts
+++ part9/ex9.1/bmiCalculator.ts
@@ -3,7 +3,7 @@ interface BmiValues {
   weight: number;
 }
 
-const calculateBmi = ( height: number, weight: number): string => {
+export const calculateBmi = ( height: number, weight: number): string => {
   const bmi = weight / Math.pow(height / 100, 2);
   if (bmi < 15) {
     return 'Very severely underweight';
--- part9/ex9.1/index.ts
+++ part9/ex9.1/index.ts
@@ -1,10 +1,31 @@
-import express from "express"
+import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack');
 });
+
+app.get('/bmi', (req, res) => {
+  let { height, weight } = req.query;
+
+  if (!height || !weight) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const heightNumber = Number(height);
+  const weightNumber = Number(weight);
+
+  if (isNaN(heightNumber) || isNaN(weightNumber)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const bmi = calculateBmi(heightNumber, weightNumber);
+
+  return res.json({ weightNumber, heightNumber, bmi });
+
+})
 
 const PORT = 3003;
 
