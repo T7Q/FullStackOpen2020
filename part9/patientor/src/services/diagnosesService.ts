fd238190c524fb024764b5d7127da45f3033d434
T7Q <tkuumola@gmail.com>
2020-12-28 20:49:14 +0200

    feat: :zap: part9 ex. 9-10

---
+++ part9/patientor/src/services/diagnosesService.ts
@@ -0,0 +1,13 @@
+import diagnosesData from '../../data/diagnoses.json'
+
+import { Diagnose } from '../types'
+
+const diagnoses: Diagnose[] = diagnosesData
+
+const getDiagnoses = (): Diagnose[] => {
+  return diagnoses;
+}
+
+export default {
+  getDiagnoses
+};
